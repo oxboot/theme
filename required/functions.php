ff733a5a415149ccda78aebe1776b19de3927b72
Zorca <vs@zorca.org>
2017-09-11 17:47:14 +0300

    PHP & Wordpress versions check added

--- required/functions.php
+++ required/functions.php
@@ -1,2 +1,24 @@
 <?php
 
+/**
+ * [en] PHP version check
+ * [ru] Проверяем версию PHP
+ */
+if (version_compare('7.0.0', phpversion(), '>=')) {
+    wp_die(
+        'You must be using PHP 7.0.0 or greater.',
+        'Invalid PHP version'
+    );
+}
+
+/**
+ * [en] Wordpress version check
+ * [ru] Проверяем версию Wordpress
+ */
+if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
+    wp_die(
+        'You must be using WordPress 4.7.0 or greater.',
+        'Invalid WordPress version'
+    );
+}
+
