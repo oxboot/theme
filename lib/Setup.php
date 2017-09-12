85ef7a53a7d61ae77e3be04a8b5c405cb294821b
Zorca <vs@zorca.org>
2017-09-12 19:33:50 +0300

    Sidebars added

--- lib/Setup.php
+++ lib/Setup.php
@@ -83,5 +83,29 @@ class Setup
              */
             add_editor_style(THEME.'/dist/css/main.css');
         }, 20);
+
+        /**
+         * Register sidebars
+         */
+        add_action('widgets_init', function () {
+            $config = [
+                'before_widget' => '<section class="widget %1$s %2$s">',
+                'after_widget'  => '</section>',
+                'before_title'  => '<h3>',
+                'after_title'   => '</h3>'
+            ];
+            register_sidebar([
+                    'name'          => __('Primary Sidebar', 'oxboot'),
+                    'id'            => 'sidebar-primary'
+                ] + $config);
+            register_sidebar([
+                    'name'          => __('Secondary Sidebar', 'oxboot'),
+                    'id'            => 'sidebar-secondary'
+                ] + $config);
+            register_sidebar([
+                    'name'          => __('Footer Sidebar', 'oxboot'),
+                    'id'            => 'sidebar-footer'
+                ] + $config);
+        });
     }
 }
