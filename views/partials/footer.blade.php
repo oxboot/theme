745357878d5d667f2ab314175ade6b9c57789ff5
Zorca <vs@zorca.org>
2017-10-06 17:54:10 +0300

    Fixed header & footer

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,7 +1,15 @@
-<header class="l-footer">
+<header class="footer">
   <div class="l-container">
     <div class="l-grid">
-      FOOTER
+      <div class="l-grid__cell l-grid__cell--align-left">
+        1
+      </div>
+      <div class="l-grid__cell">
+        2
+      </div>
+      <div class="l-grid__cell">
+        3
+      </div>
     </div>
   </div>
 </header>
--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -1,7 +1,15 @@
-<header class="l-header">
+<header class="header">
   <div class="l-container">
     <div class="l-grid">
-      HEADER
+      <div class="l-grid__cell">
+
+      </div>
+      <div class="l-grid__cell">
+
+      </div>
+      <div class="l-grid__cell">
+
+      </div>
     </div>
   </div>
 </header>
